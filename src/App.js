22a9d536175b1879c08323220975ccd90acbd3b0
Meghan Domeck <mdomeck@aol.com>
2021-02-26 11:31:17 -0500

    done 4.2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 // import AddressBook from './addressBook/AddressBook'
+import './reset.css'
 import './Default.css'
 
 import CardHooks from './addressBook/CardHooks'
@@ -28,7 +29,7 @@ class App extends Component {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about"> About</Link></li>
-            <li> <Link to="/contact"> Contact</Link></li>
+            <li><Link to="/contact"> Contact</Link></li>
           </ul>    
 
           <Route exact path="/" render={() => {
